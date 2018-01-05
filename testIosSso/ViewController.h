86dceb9a0bd7f71b72ac0610fc59918d17752317
Tseng An <catseng@sheep-tsengteki-MacBook-Pro.local>
2018-01-05 13:37:16 +0800

    Initial Commit

---
+++ testIosSso/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testIosSso
+//
+//  Created by Tseng Chiang An on 2018/1/5.
+//  Copyright © 2018年 Tseng Chiang An. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testIosSso/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testIosSso
+//
+//  Created by Tseng Chiang An on 2018/1/5.
+//  Copyright © 2018年 Tseng Chiang An. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
