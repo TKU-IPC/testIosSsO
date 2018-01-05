b01426423d976ccc72c3c9da977129e181394e7d
Tseng An <catseng@sheep-tsengteki-MacBook-Pro.local>
2018-01-05 14:16:33 +0800

    iOS 單一登入

--- testIosSso/ViewController.m
+++ testIosSso/ViewController.m
@@ -12,14 +12,30 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSURL *url = [NSURL URLWithString:@"https://sso.tku.edu.tw/ilife/Data/AndroidSsoLogin.html"];
+    //NSURLRequest負責對特定URL進行存取的請求
+    //而NSURLRequest與NSURL合作,
+    //NSURLRequest即可透過NSURL進行網頁的存取
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    //UIWebView負責顯示網頁,
+    //loadRequest:方法透過NSURLRequest取得網頁的內容,
+    //而後將網頁內容顯示在畫面上。
+    [self.displayWebView loadRequest:request];
+    //ScalesPageToFit屬性=YES,
+    //讓網頁自動調整成螢幕的大小。
+    self.displayWebView.scalesPageToFit=YES;
+    
 }
 
-
+-(void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    NSString *information = [self.displayWebView stringByEvaluatingJavaScriptFromString:@"getSsoLoginToken()"];
+    NSLog(@"Web response: %@", information);
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
